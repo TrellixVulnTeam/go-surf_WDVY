eaadd702fd9b88a7f9c0c944a48531128c0b812f
SadMearise <65180406+SadMearise@users.noreply.github.com>
2022-05-03 17:10:31 +0300

    slider

--- src/js/modules/swiper-options.js
+++ src/js/modules/swiper-options.js
@@ -1,30 +1,25 @@
 const names = ['North Shore', 'South Shore', 'West Shore ', 'East Shore']
 
-const swiper = new Swiper('.main-screen-slider', {
-	direction: 'horizontal',
-	loop: true,
-	speed: 800,
+let swiper = new Swiper(".main-screen-slider", {
+	spaceBetween: 30,
 	effect: "fade",
+	speed: 800,
+	loop: true,
+	navigation: {
+		nextEl: ".main-screen-slider__button-next",
+		prevEl: ".main-screen-slider__button-prev",
+	},
 	pagination: {
-	  el: '.main-screen-slider__pagination',
-	  clickable: true,
-	//   type: 'custom',
-	//   bulletElement: 'div',
-	//   bulletClass: 'pagination-item',
-	  renderBullet:
-	  
-	  function (index, className) {
+		el: ".main-screen-slider__pagination",
+		clickable: true,
+		renderBullet: function (index, className) {
 		return `<div class="pagination__item pagination-item ${className}">` +
 					'<span class="pagination-item__line"></span>' +
 					'<div class="pagination-item__content">' +
-						'<div class="pagination-item__number">0'+ (index + 1) +'</div>' +
-						'<div class="pagination-item__text text2">'+ names[index] +'</div>' +
+						`<div class="pagination-item__number">0${index + 1}</div>` +
+						`<div class="pagination-item__text text2">${names[index]}</div>` +
 					'</div>' +
 				'</div>'
-	}
-	},
-	navigation: {
-	  nextEl: '.main-screen-slider__button-next',
-	  prevEl: '.main-screen-slider__button-prev',
+		}
 	},
   });
