78e7856a456a35663b3e073123e777e41f69eae5
SadMearise <65180406+SadMearise@users.noreply.github.com>
2022-05-04 17:49:04 +0300

    edits

--- src/js/modules/swiper-options.js
+++ src/js/modules/swiper-options.js
@@ -1,21 +1,21 @@
 const names = ['North Shore', 'South Shore', 'West Shore ', 'East Shore']
 
-// let swiper = new Swiper(".main-screen-slider", {
-// 	effect: "fade",
-// 	speed: 800,
-// 	loop: true,
-// 	navigation: {
-// 		nextEl: ".main-screen-slider__button-next",
-// 		prevEl: ".main-screen-slider__button-prev",
-// 	},
-// 	pagination: {
-// 		el: ".main-screen-slider__pagination",
-// 		clickable: true,
-// 		renderBullet: function (index, className) {
-// 			return `<div class="pagination__item pagination-item ${className}">
-// 						<div class="pagination-item__number">0${index + 1}</div>
-// 						<div class="pagination-item__text text2">${names[index]}</div>
-// 					</div>`
-// 		}
-// 	},
-// });
+let swiper = new Swiper(".main-screen-slider", {
+	effect: "fade",
+	speed: 800,
+	loop: true,
+	navigation: {
+		nextEl: ".main-screen-slider__button-next",
+		prevEl: ".main-screen-slider__button-prev",
+	},
+	pagination: {
+		el: ".main-screen-slider__pagination",
+		clickable: true,
+		renderBullet: function (index, className) {
+			return `<div class="pagination__item pagination-item ${className}">
+						<div class="pagination-item__number">0${index + 1}</div>
+						<div class="pagination-item__text text2">${names[index]}</div>
+					</div>`
+		}
+	},
+});
